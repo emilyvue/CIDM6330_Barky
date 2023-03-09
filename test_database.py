3fce5964b67b67656cbe164dbfd980de57db7f09
Emily Vue <evue1@buffs.wtamu.edu>
2023-03-09 20:59:29 +0900

    Test Update

--- test_database.py
+++ test_database.py
@@ -81,3 +81,64 @@ def test_database_manager_add_bookmark(database_manager):
     cursor = conn.cursor()
     cursor.execute(''' SELECT * FROM bookmarks WHERE title='test_title' ''')
     assert cursor.fetchone()[0] == 1
+
+
+def test_database_manager_delete_bookmark(database_manager):
+
+    # arrange
+    database_manager.create_table(
+        "bookmarks",
+        {
+            "id": "integer primary key autoincrement",
+            "title": "text not null",
+            "url": "text not null",
+            "notes": "text",
+            "date_added": "text not null",
+        },
+    )
+
+    data = {
+        "title": "test_title",
+        "url": "http://example.com",
+        "notes": "test notes",
+        "date_added": datetime.utcnow().isoformat()
+    }
+
+    # act
+    database_manager.delete("bookmarks", data)
+    # assert
+    conn = database_manager.connection
+    cursor = conn.cursor()
+    cursor.execute(''' SELECT * FROM bookmarks WHERE title = "test_title" ''')
+    assert cursor.fetchone() == None
+
+
+def test_database_manager_select_bookmark(database_manager):
+
+    # arrange
+    database_manager.create_table(
+        "bookmarks",
+        {
+            "id": "integer primary key autoincrement",
+            "title": "text not null",
+            "url": "text not null",
+            "notes": "text",
+            "date_added": "text not null",
+        },
+    )
+
+    data = {
+        "title": "test_title",
+        "url": "http://example.com",
+        "notes": "test notes",
+        "date_added": datetime.utcnow().isoformat()
+    }
+
+    # act
+    database_manager.add("bookmarks", data)
+    database_manager.select("bookmarks", data)
+    # assert
+    conn = database_manager.connection
+    cursor = conn.cursor()
+    cursor.execute(''' SELECT * FROM bookmarks WHERE title='test_title' ''')
+    assert cursor.fetchone()[0] == 1
